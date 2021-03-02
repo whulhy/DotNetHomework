84b70526973209c7e104bf627761e0000f3d588d
whulhy <992878147@qq.com>
2021-03-02 22:50:07 +0800

    Homework

---
+++ Homework1/Homework1.2/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework1._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void elementHost1_ChildChanged(object sender, System.Windows.Forms.Integration.ChildChangedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    textBox3.Text = Convert.ToString(Int32.Parse(textBox1.Text) + Int32.Parse(textBox2.Text));
+                    break;
+                case 1:
+                    textBox3.Text = Convert.ToString(Int32.Parse(textBox1.Text) - Int32.Parse(textBox2.Text));
+                    break;
+                case 2:
+                    if (textBox2.Text == "0") { textBox3.Text = "除数不能为0";break ; }
+                    textBox3.Text = Convert.ToString(Int32.Parse(textBox1.Text) * Int32.Parse(textBox2.Text));
+                    break;
+                case 3:
+                    textBox3.Text = Convert.ToString(Int32.Parse(textBox1.Text) / Int32.Parse(textBox2.Text));
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input first integer");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Please input second integer");
+            int b = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Please input symbol");
+            char  s = Convert.ToChar (Console.ReadLine());
+            switch (s)
+            {
+                case '+':
+                    Console.WriteLine($"结果为：{a + b}");
+                    break;
+                case '-':
+                    Console.WriteLine($"结果为：{a - b}");
+                    break;
+                case '*':
+                    Console.WriteLine($"结果为：{a * b}");
+                    break;
+                case '/':
+                    Console.WriteLine($"结果为：{a / b}");
+                    break;
+                default:
+                    Console.WriteLine("符号错误");
+                    break;
+            }
+
+        }
+    }
+}
