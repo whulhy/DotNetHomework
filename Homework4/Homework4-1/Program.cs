1cb285dad723394c959eab2c86fef53a0d346853
whulhy <992878147@qq.com>
2021-03-24 22:35:28 +0800

    Homework4

---
+++ Homework4/Homework4-1/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4_1
+{
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public  void Print(int a )
+        {
+            Console.WriteLine(a);
+        }
+        public void ForEach(GenericList<T> a,Action<T> action)
+        {
+            Node<T> node = a.Head;
+            while (node !=null)
+            {
+                action(node.Data);
+                node = node.Next;
+            }
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> array= new GenericList<int>();
+            Action<int> action1 = array.Print;
+            Random random = new Random();
+            int sum = 0;
+            int Maxvalue = 0;
+            int Minvalue = int.MaxValue;
+            Action<int> action2 = delegate (int a)
+            {
+                if (a > Maxvalue)
+                {
+                    Maxvalue = a;
+                }
+                if (a < Minvalue)
+                {
+                    Minvalue = a;
+                }
+            };
+            for (int i = 0; i < 10; i++)
+            {
+                array.Add((random.Next(20)));
+            }
+            array.ForEach(array, s => sum += s);
+            array.ForEach(array, action1);
+            array.ForEach(array, action2);
+            Console.WriteLine($"和为{sum }");
+            Console.WriteLine($"最大值为{Maxvalue},最小值为{Minvalue}");
+
+        }
+    }
+}
---
+++ Homework4/Homework4-2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Homework4_2
+{
+    public delegate void Tick(object sender);
+    public delegate void BellTime(object sender, clockTime args);
+    public class clockTime
+    {
+        public int hour { get; set; }
+        public int minute { get; set; }
+        public int seconds { get; set; }
+    }
+    public class Clock
+    {
+        public event Tick onTick;
+        public event BellTime setBell;
+        public void Start()
+        {
+            onTick(this);
+        }
+
+    }
+    public class Form
+    {
+        private int Hour = 0;
+        private int Minute = 0;
+        private int Second = 0;
+        public void setBell(int a,int b,int c)
+        {
+            Hour = a;
+            Minute = b;
+            Second = c;
+        }
+        public Clock clock = new Clock();
+        public Form()
+        {
+            clock.onTick += Clo_Bell;
+            clock.onTick += Clo_tick;
+        }
+        void Clo_Bell(object sender)
+        {
+            Console.WriteLine($"设定时间为{Hour}时{Minute}分{Second}秒");
+        }
+        void Clo_tick(object sender)
+        {
+            while (!(System.DateTime.Now.Hour == Hour && System.DateTime.Now.Minute == Minute && System.DateTime.Now.Second == Second))                
+            {
+                Console.WriteLine($"{System.DateTime.Now.Hour}时{System.DateTime.Now.Minute}分{System.DateTime.Now.Second}秒");
+                System.Threading.Thread.Sleep(1000); 
+            }
+            if(System.DateTime.Now.Hour == Hour && System.DateTime.Now.Minute == Minute && System.DateTime.Now.Second == Second){
+                Console.WriteLine("设定的时间到了");
+            }
+
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form form1 = new Form();
+            form1.setBell(23,27,30);
+            form1.clock.Start();
+        }
+    }
+}
