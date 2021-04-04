6c32688b3525d35237e93f311d3a50c20accca81
whulhy <992878147@qq.com>
2021-04-04 23:55:44 +0800

    Homework6

---
+++ Homework6/Homework5/OrderService.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Homework5
+{
+    [Serializable]
+   public class OrderService 
+    {
+       public List<Order> order = new List<Order>();
+       XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Order>));
+
+        public OrderService() { }
+        public void AddItems(Order p)
+        {
+            Boolean Isadd = false;
+            if (p == null) Isadd = true;
+            foreach (var temp in order)
+            {
+                if (temp.Equals(p))
+                {
+                    Console.WriteLine("此订单号已经存在");
+                    Isadd = true;
+                }
+            }
+            if (Isadd == false) order.Add(p);
+        }
+
+        public void Remove(int a)
+        {
+            Boolean SuccessRemove = false;
+            for (int i = 0; i < order.Count; i++)
+            {
+                if (order[i].OrderNum == a)
+                {
+                    order.Remove(order[i]);
+                    SuccessRemove = true;
+                }
+            }
+            if (SuccessRemove == true)
+            {
+                Console.WriteLine($"订单号为{a}的商品移除成功");
+            }
+            else Console.WriteLine($"找不到订单号为{a}的商品，移除失败");
+        }
+
+        public override string ToString()
+        {
+            string a = "";
+            foreach (var temp in order)
+            {
+                a = a + temp.ToString()+ " 总金额为"+temp.Sum() + "\n\n";
+            }
+            return a;
+        }
+        
+        public List<Order> SelectID(int a)
+        {
+            if (a > order.Count||a<0) { 
+                Console.WriteLine("输入的订单号不存在"); 
+                return null; }
+            else
+            {
+                var query = from s in order
+                            where a == s.OrderNum
+                            orderby s.Sum()
+                            select s;
+                List<Order> list = query.ToList();
+                foreach (var temp in list)
+                {
+                    Console.WriteLine(temp.ToString()+ "总金额为" + temp.Sum()+ "\n");
+                }
+                return list;
+            }
+            
+        }
+
+        public List<Order> SelectSum(int a)
+        {          
+                var query = from s in order
+                            where a == s.Sum()
+                            orderby s.Sum()
+                            select s;
+                List<Order> list = query.ToList();
+            if (list.Count == 0) { Console.WriteLine("找不到指定金额的订单"); return null; }
+            else
+            {
+                foreach (var temp in list)
+                {
+                    Console.WriteLine(temp.ToString() + "总金额为" + temp.Sum() + "\n");
+                }
+            }
+            return list;
+        }
+
+        public List<Order> SelectName(string  a)
+        {
+            var query = from s in order
+                        where a == s.Customer || s.SelectGoods(a)                            
+                        orderby s.Sum()
+                        select s;
+            List<Order> list = query.ToList();
+            if (list.Count == 0) { Console.WriteLine("找不到指定名称的订单"); return null; }
+            else
+            {
+                foreach (var temp in list)
+                {
+                    Console.WriteLine(temp.ToString() + "总金额为" + temp.Sum() + "\n");
+                }
+            }
+            return list;
+        }
+        
+        public void Exports()
+        {
+            using (FileStream fileStream = new FileStream("s.xml", FileMode.Create))
+            {
+                xmlSerializer.Serialize(fileStream, order);
+            }
+            Console.WriteLine("序列化完成");
+            Console.WriteLine(File.ReadAllText("s.xml"));
+        }
+        public string  Imports()
+        {
+            string a = "";
+            using (FileStream fs = new FileStream("s.xml", FileMode.Open))
+            {
+                Console.WriteLine("反序列化");
+                List<Order> orders = (List<Order>)xmlSerializer.Deserialize(fs);
+                foreach(var temp in orders)
+                {
+                     a = a + temp; 
+                }
+            }
+            return a;
+        }
+    }
+    }
+
---
+++ Homework6/Homework5Tests/OrderServiceTests.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Homework5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Homework5.Tests
+{
+    [TestClass()]
+    public class OrderServiceTests
+    {
+        [TestMethod()]
+        public void AddItemsTest()
+        {
+            OrderService service = new OrderService();
+            Order order = new Order(0, "Person", "12:00", "Any");
+            service.AddItems(order);
+            CollectionAssert.Contains(service.order,order);
+        }
+        [TestMethod()]
+        public void AddItemsTest2()
+        {
+            OrderService service = new OrderService();
+            Order order = new Order(0, "Person", "12:00", "Any");
+            service.AddItems(order);
+            service.AddItems(order);
+            CollectionAssert.AllItemsAreUnique(service.order);
+        }
+        [TestMethod()]
+        public void AddItemsTest3()
+        {
+            OrderService service = new OrderService();
+            Order order = null;
+            service.AddItems(order);
+            CollectionAssert.AllItemsAreNotNull(service.order);
+        }
+            [TestMethod()]
+        public void RemoveTest()
+        {
+            OrderService service = new OrderService();
+            Order order1 = new Order(0, "Person", "12:00", "Any");
+            Order order2 = new Order(1, "Person2", "12:00", "Any");
+            service.AddItems(order1);
+            service.AddItems(order2);
+            service.Remove(0);
+            CollectionAssert.DoesNotContain(service.order,order1);
+            service.Remove(3);
+            CollectionAssert.Contains(service.order, order2);
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            OrderService service = new OrderService();
+            Order order1 = new Order(0, "Person", "12:00", "Any");
+            service.AddItems(order1);
+            string a = order1.ToString()+ " 总金额为" + order1.Sum() + "\n\n";
+            Assert.AreEqual(service.ToString(), a);
+        }
+
+        [TestMethod()]
+        public void SelectIDTest()
+        {
+            OrderService service = new OrderService();
+            Order order1 = new Order(0, "Person", "12:00", "Any");
+            Order order2 = new Order(1, "Person2", "12:00", "Any");
+            service.AddItems(order1);
+            service.AddItems(order2);
+            List<Order> Testlist = new List<Order>() { order1};
+            CollectionAssert.AreEqual(Testlist, service.SelectID(0));
+            Assert.IsNull(service.SelectID(3));
+            
+        }
+
+        [TestMethod()]
+        public void SelectSumTest()
+        {
+            OrderService service = new OrderService();
+            Order order1 = new Order(0, "Person", "12:00", "Any");
+            Order order2 = new Order(1, "Person2", "12:00", "Any");
+            OrderDetails g1 = new OrderDetails("笔", 5.0, 1);
+            OrderDetails g2 = new OrderDetails("本子", 10.0, 1);
+            order1.AddItems(g1);
+            order2.AddItems(g2);
+            service.AddItems(order1);
+            service.AddItems(order2);
+            List<Order> Testlist = new List<Order>() { order1 };
+            CollectionAssert.AreEqual(Testlist, service.SelectSum(5));
+            Assert.IsNull(service.SelectSum(15));
+        }
+
+        [TestMethod()]
+        public void SelectNameTest()
+        {
+            OrderService service = new OrderService();
+            Order order1 = new Order(0, "Person", "12:00", "Any");
+            Order order2 = new Order(1, "Person2", "12:00", "Any");
+            service.AddItems(order1);
+            service.AddItems(order2);
+            List<Order> Testlist = new List<Order>() { order1};
+            CollectionAssert.AreEqual(Testlist, service.SelectName("Person"));
+            Assert.IsNull(service.SelectName("Person3"));
+        }
+        [TestMethod()]
+        public void ImportTest()
+        {
+            OrderService service = new OrderService();
+            Order order1 = new Order(0, "Person", "12:00", "Any");
+            service.AddItems(order1);
+            service.Exports();
+            string a = service.Imports();
+            Assert.AreEqual(a, order1.ToString());
+          
+        }
+
+    }
+}
